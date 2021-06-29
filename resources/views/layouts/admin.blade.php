f7154eb909b880a4be9a696629e934b3a3cf4d0e
setiyawan12 <yayangsetiyawan09@gmail.com>
2021-06-29 22:36:37 +0700

    8. Set CSS layout

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -32,7 +32,7 @@
 <div class="wrapper">
 
     <!-- Navbar -->
-    <nav class="main-header navbar navbar-expand navbar-white navbar-light">
+    <nav class="main-header navbar navbar-expand navbar-primary navbar-light">
         <!-- Left navbar links -->
         <ul class="navbar-nav">
             <li class="nav-item">
@@ -61,12 +61,12 @@
     <!-- /.navbar -->
 
     <!-- Main Sidebar Container -->
-    <aside class="main-sidebar sidebar-dark-primary elevation-4">
+    <aside class="main-sidebar background-color:pink elevation-4">
         <!-- Brand Logo -->
         <a href="index3.html" class="brand-link">
             <img src="dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
                  style="opacity: .8">
-            <span class="brand-text font-weight-light">Toko Online</span>
+            <span class="brand-text font-weight-light">Toko Buku</span>
         </a>
 
         <!-- Sidebar -->
@@ -147,14 +147,7 @@
     <div class="content-wrapper">
         @yield('content')
     </div>
-    <!-- /.content-wrapper -->
-    <footer class="main-footer">
-        <strong>Copyright &copy; 2014-2019 <a href="http://adminlte.io">AdminLTE.io</a>.</strong>
-        All rights reserved.
-        <div class="float-right d-none d-sm-inline-block">
-            <b>Version</b> 3.0.5
-        </div>
-    </footer>
+    <!-- /.content-wrapper -->  
 
     <!-- Control Sidebar -->
     <aside class="control-sidebar control-sidebar-dark">
